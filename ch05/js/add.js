371e8191822c7ae2a82f1cc90dcd2ca1169f3c05
azureun <euny1387@gmail.com>
2025-01-27 19:13:52 +0900

    ch05-02 fin

--- ch05/js/add.js
+++ ch05/js/add.js
@@ -1,4 +1,12 @@
-function addNumber() {
-    var sum = 10+20;
-    console.log(sum);
+/*
+// 두 수를 입력 받아 더한 결과 리턴 함수
+var num1 = parseInt(prompt("첫번째 숫자는?"));
+var num2 = parseInt(prompt("두번째 숫자는?"));
+var result = addNumber(num1, num2);
+alert("두 수를 더한 값은 " + result + "입니다.");
+
+function addNumber(a, b) {
+    var sum = a + b;
+    return sum;
 }
+*/
---
+++ ch05/js/prac.js
@@ -0,0 +1,34 @@
+// 지역 변수 & 전역 변수
+/*
+var myVar = 100;    //전역 변수 선언
+test();
+test2();
+function test() {
+    var myVar = 50; //지역 변수 선언
+    document.write("myVar is " + myVar + "<br>");
+}
+
+function test2() {
+    myVar = 550;     //전역 변수 선언
+    document.write("myVar is " + myVar + "<br>");
+}
+*/
+
+// 익명 함수 & 즉시 실행 함수
+//익명 함수
+var add = function(a, b) {  //익명함수 - 함수 선언 후, add 변수에 할당.
+    return a + b;
+}
+var sum = add(10, 20);      //익명 함수 실행 후, 결과값을 변수 sum에 저장
+console.log(sum);           //sum 변수 확인
+
+//즉시 실행 함수
+var result = (function(){
+    return 10 + 20;
+}());
+console.log(result);
+
+var result2 = (function(a, b){  //매개 변수 추가
+    return a + b;
+}(10, 20));             //인수 추가
+console.log(result2);
