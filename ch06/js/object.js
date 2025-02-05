39aea8d089b64e293ed10b21ae3f8e16e3e4762e
azureun <euny1387@gmail.com>
2025-02-05 23:22:01 +0900

    ch06-2 fin

--- ch06/js/object.js
+++ ch06/js/object.js
@@ -7,9 +7,6 @@ var toyRobot = {            //toyRobot 객체를 선언한 후 속성과 함수 
     showStock: function() {     //showStock 함수 정의
         document.querySelector('#display').innerHTML = this.name + " 제품은 " + this.quantity +  "개 남아있습니다.";
     },
-    showPrice: function(){
-        document.querySelector('#display').innerHTML = this.name + " 제품의 가격은 " + this.price + "입니다.";
-    }
 };
 
-toyRobot.showPrice();
+toyRobot.showStock();       //toyRobot 객체의 showStock() 함수 실행
