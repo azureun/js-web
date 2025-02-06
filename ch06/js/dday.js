a53bf5e708344d94f67fed2e22c9d265cab6e79e
azureun <euny1387@gmail.com>
2025-02-06 12:49:45 +0900

    ch06-03 fin

---
+++ ch06/js/dday.js
@@ -0,0 +1,24 @@
+var now = new Date();
+
+var firstDate = String(prompt("처음 만난 날짜를 입력하세요.(YYYY-MM-DD)"));
+var firstDay = new Date(firstDate); //처음 만난 날 설정
+var toNow = now.getTime();         //오늘 날짜를 밀리초로 바꿈
+var toFirst = firstDay.getTime();  //처음 만난 날을 밀리초로 바꿈
+var passedTime = toNow - toFirst;   //처음 만난 날과 오늘 사이의 차이 (밀리초)
+var passedDay = Math.round(passedTime/(24*60*60*1000)); //밀리초 -> 일(day) 변환 후 반올림
+document.querySelector('#accent').innerHTML = passedDay + "일";
+
+calcDate(100);  //100일 기념일 계산해서 표시
+calcDate(200);  //200일 기념일 계산해서 표시
+calcDate(365);  //1년 기념일 계산해서 표시
+calcDate(500);  //500일 기념일 계산해서 표시
+
+function calcDate(days){
+    var future = toFirst + days*(1000*60*60*24);
+    var someday = new Date(future);
+    var year = someday.getFullYear();
+    var month = someday.getMonth() + 1; //getMonth는 0~11까지이므로 주의
+    var date = someday.getDate();
+    document.querySelector("#date" + days).innerText = year + "년" + month + "일" + date + "일";
+}
+
