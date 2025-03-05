0f672d847a2aa90d3486ab62a53adf4fd631f219
azureun <euny1387@gmail.com>
2025-03-05 23:12:30 +0900

    ch08-5 fin

--- ch08/js/product.js
+++ ch08/js/product.js
@@ -1,9 +1,25 @@
-var cup = document.querySelector('#cup');    //id="view"인 요소 가져옴.
-var smallPics = document.querySelectorAll(".small");    
+var isOpen = false;
+var bigPic = document.querySelector("#cup");  
+var smallPics = document.querySelectorAll(".small");  
 
 for(i=0; i<smallPics.length; i++) {
-    smallPics[i].addEventListener("click", function() { //view를 눌렀을 때 실행할 함수 선언
-        //document.querySelector('#detail').style.display = "block";  //detail 요소의 display 속성 > block 설정
-
-    })
+	smallPics[i].addEventListener("click", function() {
+		newPic = this.src;
+		bigPic.setAttribute("src", newPic);
+	});
 }
+
+
+var view = document.querySelector("#view");  // #view 요소를 가져옴
+view.addEventListener("click", function() {
+	if (isOpen == false) {  // 상세 정보가 감춰져 있다면 
+		document.querySelector("#detail").style.display = "block";  // 상세 정보를 화면에 표시
+		view.innerHTML = "상세 설명 닫기";  // 링크 내용 수정
+		isOpen = true;  // 표시 상태로 지정
+	}
+	else {  // 상세 정보가 표시되어 있다면 
+		document.querySelector("#detail").style.display = "none";  // 상세 정보를 화면에서 감춤
+		view.innerHTML = "상세 설명 보기";  // 링크 내용 수정
+		isOpen = false;  // 감춰진 상태로 지정
+	}
+});
