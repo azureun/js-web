daf4d695c9f266d69379233d3a4860a031405acf
azureun <euny1387@gmail.com>
2025-03-06 21:24:21 +0900

    오타 수정

--- ch08/js/domcss.js
+++ ch08/js/domcss.js
@@ -7,6 +7,6 @@ myRect.addEventListener("mouseover", function() { 	// 도형에서 마우스가 
 
 myRect.addEventListener("mouseout", function() {	// 도형에서 마우스가 떨어지면 실행
 	myRect.style.backgroundColor = "";	// myRect 요소 배경색 지우기
-	myRect.style.borderRadiaus = "";	// myRect 요소 테두리 둥글게 처리 안 함함
+	myRect.style.borderRsdius = "";	// myRect 요소 테두리 둥글게 처리 안 함함
 });
 
