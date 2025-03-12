dec510207a8c7c61674e4c3265a2bcf7da44e1a0
azureun <euny1387@gmail.com>
2025-03-12 19:29:12 +0900

    ch08 연습문제 fin

--- ch08/js/quiz-1.js
+++ ch08/js/quiz-1.js
@@ -1,6 +1,6 @@
 var myText = document.querySelector("#myText");
 
-myText.addEventListener("click", function(){
+myText.addEventListener("click", function() {
     myText.style.fontSize = "20px";
     myText.style.color = "blue";
     myText.style.backgroundColor = "#ccc";
